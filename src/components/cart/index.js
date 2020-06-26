77f342a80ac7ecad121b65b20fe4f577400fb023
tverdone10 <tverdone10@gmail.com>
2020-06-26 14:32:52 -0700

    connected to stripe

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,26 +1,94 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 
 const items = [
-    {skey: "sku", quantity: 1, price: 99999}
-]
+  {
+    price: "price_1GxhjbFFxFG9mmf6Wt9x7oPr",
+    quantity: 1,
+    cost: 35900,
+    name: "Squier Jaguar Bass",
+  },
+  {
+    price: "price_1GxhhTFFxFG9mmf68RIKzNB6",
+    quantity: 1,
+    cost: 75000,
+    name: "Fender Telecaster",
+  },
+  {
+    price: "price_1GxhgBFFxFG9mmf6UXKrNh4D",
+    quantity: 1,
+    cost: 90000,
+    name: "Gibson Explorer",
+  },
+];
 
-export default function Cart ({stripeToken}) {
-    return <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Image</th>
-                        <th>Quantity</th>
-                        <th>price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        
-                    }
-                </tbody>
-            </table>
+function totalPrice(items) {
+  return items.reduce(
+    (accumulated, item) => accumulated + item.quantity * item.cost,
+    0.0
+  );
+}
 
-        </div>
+
+
+export default function Cart({ stripeToken }) {
+  const [stripe, setStripe] = useState(null);
+
+  useEffect(() => {
+    if (window.Stripe) setStripe(window.Stripe(stripeToken));
+  }, [stripeToken]);
+
+  function checkOut() {
+    stripe.redirectToCheckout({
+      lineItems: items.map(item => ({
+        price: item.price,
+        quantity: item.quantity
+      })),
+      successUrl: 'https://your-website.com/success',
+      cancelUrl: 'https://your-website.com/canceled'
+  
+    })
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Quantity</th>
+            <th>price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr>
+              <td>{item.name}</td>
+              <td>
+                <img
+                  src={`/images/${item.price}.jpg`}
+                  alt={item.name}
+                  width={50}
+                />
+              </td>
+              <td>{item.quantity}</td>
+              <td>{`$` + (item.cost * 0.01).toFixed(2)}</td>
+            </tr>
+          ))}
+          <tr>
+            <td style={{ textAlign: "right" }} colSpan={3}>
+              Total:
+            </td>
+            <td>{totalPrice(items)}</td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: "right" }} colSpan={4}>
+              <button onClick={checkOut}>Checkout with Stripe</button>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Cart from "./components/cart"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cart />
+    <Cart  stripeToken="pk_test_51GxgS5FFxFG9mmf6PK2Eash6BxpdphmOfgpQSEhqIWGkH2ScSzvnWmm3Ah5wrHfyzfjuqPYM5n3sizpubL4T745g009AkWM92D"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
